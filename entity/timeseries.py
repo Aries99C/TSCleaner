16be0ee9adcb1c9cc2709a8f17afec6c0cb0b6e5
Aries99C <47294484+Aries99C@users.noreply.github.com>
2023-07-07 21:31:22 +0800

    深入分析了基于多个复杂约束修复的情况

--- entity/timeseries.py
+++ entity/timeseries.py
@@ -148,10 +148,8 @@ if __name__ == '__main__':
 
     print(fan.len)
     print(fan.dim)
-    # fan.clean.plot(subplots=True, figsize=(6, 4),
-    #                style=['+--', 'o--', '*--'], ms=3, linewidth=1, markerfacecolor='white')
 
-    fig = plt.figure(figsize=(10, 6))
+    fig = plt.figure(1, figsize=(10, 6))
     ax = Axes3D(fig, auto_add_to_figure=False)
     fig.add_axes(ax)
     # ['U3_HNV10CT102', 'U3_HNV10CT103', 'U3_HNV10CT104']
@@ -159,8 +157,12 @@ if __name__ == '__main__':
     y = fan.clean['U3_HNV10CT103'].values
     z = fan.clean['U3_HNV10CT104'].values
 
-    ax.scatter(x, y, z, c='g', label='正确值')
-    ax.scatter(x[100:150]+0.07, y[100:150], z[100:150], c='r', label='错误值')
+    ax.scatter(x[:100], y[:100], z[:100], c='g')
+    ax.scatter(x[120:], y[120:], z[120:], c='g')
+    ax.scatter(x[100:120]+0.1, y[100:120], z[100:120], c='r', label='错误值')
+    ax.scatter(np.add(x[100:120]+0.02, np.array([random.random()*0.02 for i in range(20)])), y[100:120], z[100:120],
+               c='b', label='修复值')
+    ax.scatter(x[100:120], y[100:120], z[100:120], c='brown', label='正确值')
     ax.set_xlabel('U3_HNV10CT102')
     ax.set_ylabel('U3_HNV10CT103')
     ax.set_zlabel('U3_HNV10CT104')
@@ -177,5 +179,57 @@ if __name__ == '__main__':
     linepts += data_mean
     ax.plot3D(*linepts.T)
 
+    fig2 = plt.figure(figsize=(6, 4))
+    t = list(fan.clean.index)[80:140]
+
+    ax1 = plt.subplot(311)
+    x_e = x.copy()
+    x_e[100:120] += 0.1
+    ax1.plot(t, x_e[80:140], 'o-', ms=3, linewidth=1, markerfacecolor='white', c='darkorange', label='U3_HNV10CT102')
+    plt.ylim(28.8, 29.1)
+    plt.tick_params('x', labelbottom=False)
+
+    ax2 = plt.subplot(312, sharex=ax1)
+    ax2.plot(t, y[80:140], 'o-', ms=3, linewidth=1, markerfacecolor='white', c='palegreen', label='U3_HNV10CT103')
+    plt.ylim(36.5, 37.2)
+    plt.tick_params('x', labelbottom=False)
+
+    ax3 = plt.subplot(313, sharex=ax1)
+    ax3.plot(t, z[80:140], 'o-', ms=3, linewidth=1, markerfacecolor='white', c='skyblue', label='U3_HNV10CT104')
+    plt.ylim(32.2, 32.9)
+    plt.xticks(rotation=45)
+    plt.tick_params('x', labelsize=8)
+
+    plt.tight_layout()
     plt.show()
-    # print(error(fan))
+
+    fig3 = plt.figure(figsize=(5, 5))
+    ct102 = fan.clean['U3_HNV10CT102'].values[80:140]
+    ct103 = fan.clean['U3_HNV10CT103'].values[80:140]
+
+    delta = ct103 - ct102
+    mean = np.mean(delta)
+    std = np.std(delta)
+
+    for i in range(len(ct102)):
+        if not (ct102[i] + mean - 2 * std <= ct103[i] <= ct102[i] + mean + 2 * std):
+            print(i)
+
+    plt.scatter(ct102[:59], ct103[:59], marker='o', s=7, c='white', edgecolors='g')
+    plt.scatter(ct102[60:], ct103[60:], marker='o', s=7, c='white', edgecolors='g')
+    plt.scatter(ct102[59], ct103[59], marker='o', s=7, c='white', edgecolors='r')
+
+    x = np.linspace(28.9, 28.96, 100)
+    y1 = x + mean + 2 * std
+    y2 = x + mean - 2 * std
+
+    plt.plot(x, y1, '--', c='blue')
+    plt.plot(x, y2, '--', c='blue')
+    plt.fill_between(x, y1, y2, where=y1>y2, color='grey', alpha=0.5)
+
+    plt.grid(True, linestyle='-.')
+    plt.xlim(28.9, 28.96)
+    plt.ylim(36.86, 36.98)
+    plt.xlabel('U3_HNV10CT102')
+    plt.ylabel('U3_HNV10CT103')
+    plt.show()
